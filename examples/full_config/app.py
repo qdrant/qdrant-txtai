6dbf32ef7d697891003c898590b3c0687debf9b7
Kacper Łukawski <lukawski.kacper@gmail.com>
2023-01-18 14:38:35 +0100

    Bump qdrant-client to 0.11.1

--- examples/full_config/app.py
+++ examples/full_config/app.py
@@ -1,7 +1,7 @@
 from txtai.app import Application
 from txtai.embeddings import Embeddings
 
-config = Application.read("./app.yml")
+config = Application.read("./app-cloud.yml")
 embeddings = Embeddings(config["embeddings"])
 embeddings.index([(0, "Correct", None), (1, "Not what we hoped", None)])
 result = embeddings.search("positive", 1)
